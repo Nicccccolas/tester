93a6453116d469c36c0d094896518f0abc750e92
Nicccccolas <nicolaspantojadi@gmail.com>
2022-11-20 22:59:11 -0500

    mobile commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import './styles/header.css'
 
 const Header = () => {
   return (
-    <header className='title'>
-      simula<span className='title_span'>cr.</span>
+    <header className='header-container'>
+      <h1 className='title'>simula<span className='title_span'>cr.</span></h1>
+      <i className='bx bx-menu menu__icon menu--open'></i>
     </header>
   )
 }
--- src/components/Tareas.jsx
+++ src/components/Tareas.jsx
@@ -5,7 +5,10 @@ const Tareas = ({pregunta, index}) => {
   return (
     <div className='task-container'>
       <h3 className='task_title'>Tarea: {index+1}</h3>
-      <p className='task_item'>{pregunta.texto}</p>
+      <div 
+      className='task_item'
+      dangerouslySetInnerHTML={{__html:pregunta.texto}}
+      />
       <p className='task_time'>Duracion de la tarea: {pregunta.tiempo}</p>
     </div>
   )
